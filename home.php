c60ec30001f258817b4f698abbe7d0854375cf40
khurin15 <khurinrahmadini@gmail.com>
2020-11-30 15:22:06 +0700

    project pertamaku

---
+++ home.php
@@ -0,0 +1,22 @@
+<?php  //tag
+//ini komentar
+//strongly type variables
+//int a = 1;
+//variable apapaun harus diikuti dengan dolar
+//String nama = "khurin"
+//weakly typed
+//jangan lupa harus ada tag buka dan tag tutup
+echo "<h1>Welcome home</h1>";
+echo "aku lapar";
+
+$a = 1;
+$nama = "khurin";
+?>
+<?php
+
+for($i=0; $i<10; $i++)
+{
+    echo "<h1>Halo</h1>";
+}
+
+?>
